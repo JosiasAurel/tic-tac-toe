1e131a6d4932a3126a3bc01796c5c6d276b0d317
JosiasAurel <ndjosiasaurel@gmail.com>
2021-12-17 17:14:26 +0100

    add cors middleware

--- web/server/main.py
+++ web/server/main.py
@@ -1,12 +1,31 @@
 
-from fastapi import FastAPI, Response
-from pysondb import db
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from deta import Deta
 
-ttt = db.getDb("ttt.json")
+deta = Deta("b0idndtb_7T1ci58n9UfVPmzrfhCV28F4gShBD5Yb")
+
+game_db = deta.Base("tic-tac-toe")
 
 app = FastAPI()
 
+# add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.route("/")
+
+@app.get("/")
 def _root():
     return {"msg": "Hello World"}
+
+
+@app.post("/push-state")
+async def _push_state(req: Request):
+    body = await req.json()
+    print(body)
+    return
